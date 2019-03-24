437b1ae48c5ac9633da5286b5d5a09533e6fc96c
ViniciusLuciano <vinilucianoo@gmail.com>
2019-03-24 13:04:14 -0300

    arvore binaria

---
+++ arvoreBin.c
@@ -0,0 +1,108 @@
+#include "arvoreBin.h"
+
+ArvoreBin* iniciaArvore() {
+    ArvoreBin *raiz = (ArvoreBin*)malloc(sizeof(ArvoreBin));
+    if(raiz != NULL) {
+        *raiz = NULL;
+    }
+    return raiz;
+}
+
+void liberarNode(struct Node* node) {
+    if(node == NULL)
+        return;
+    liberarNode(node->esq);
+    liberarNode(node->dir);
+    free(node);
+    node = NULL;
+}
+
+void liberarArvore(ArvoreBin *raiz) {
+    if(raiz == NULL)
+        return;
+    liberarNode(*raiz);
+    free(raiz);
+}
+
+int estaVazia(ArvoreBin *raiz) {
+    if(raiz == NULL)
+        return 1;
+    if((*raiz) == NULL)
+        return 1;
+    return 0;
+}
+
+int adicionarElemento(ArvoreBin *raiz, Forma *forma) {
+    if(raiz == NULL)
+        return 0;
+
+    struct Node* novo;
+    novo = (struct Node*)malloc(sizeof(struct Node));
+    if(novo == NULL)
+        return 0;
+
+    novo->forma = forma;
+    novo->dir = NULL;
+    novo->esq = NULL;
+
+    if((*raiz) == NULL)
+        (*raiz) = novo;
+    else {
+        struct Node* atual = (*raiz);
+        struct Node* ant = NULL;
+        while(atual != NULL) {
+            
+            if(forma->id == atual->forma->id) {
+                novo->dir = atual->dir;
+                novo->esq = atual->esq;
+                if(ant != NULL) {
+                    if(forma->id > ant->forma->id) {
+                        ant->dir = novo;
+                    } else {
+                        ant->esq = novo;
+                    }
+                } else {
+                    (*raiz) = novo;
+                }
+                free(atual);
+                return 1;
+            }
+
+            ant = atual;
+            if(forma->id > atual->forma->id) {
+                atual = atual->dir;
+            } else {
+                atual = atual->esq;
+            }
+
+        }
+        if(forma->id > ant->forma->id) {
+            ant->dir = novo;
+        } else {
+            ant->esq = novo;
+        }
+    }
+    return 1;
+}
+
+Forma* encontrarForma(ArvoreBin *raiz, int id) {
+    if(raiz == NULL) {
+        return NULL;
+    }
+    
+    struct Node* atualNode = (*raiz);
+    while(atualNode != NULL) {
+        if(id == atualNode->forma->id) {
+            return atualNode->forma;
+        }
+        
+        if(id > atualNode->forma->id)
+            atualNode = atualNode->dir;
+        else
+            atualNode = atualNode->esq;
+    }
+
+    if(atualNode == NULL) {
+        return NULL;
+    }
+}
---
+++ arvoreBin.h
@@ -0,0 +1,25 @@
+#ifndef ARVOREBIN_H
+#define ARVOREBIN_H
+
+#include "formas.h"
+#include "headers.h"
+
+typedef struct Node {
+    Forma* forma;
+    struct Node *esq;
+    struct Node *dir;
+} Node;
+
+typedef struct Node* ArvoreBin;
+
+ArvoreBin* iniciaArvore();
+
+void liberarArvore(ArvoreBin* raiz);
+
+int estaVazia(ArvoreBin* raiz);
+
+int adicionarElemento(ArvoreBin *raiz, Forma *forma);
+
+Forma* encontrarForma(ArvoreBin *raiz, int id);
+
+#endif
