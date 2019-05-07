db657572443a89fc0607b47054a697cc5b8869ad
ViniciusLuciano <vini-luciano@hotmail.com>
2019-05-06 22:23:09 -0300

    recolocando Geometria e Colisao

---
+++ t2/src/Utils/Geometria/geometria.h
@@ -0,0 +1,18 @@
+#ifndef GEOMETRIA_H
+#define GEOMETRIA_H
+
+#include "../../headers.h"
+#include "./Objetos/Circulo/circulo.h"
+#include "./Objetos/Retangulo/retangulo.h"
+#include "./Objetos/Forma/forma.h"
+
+bool pontoInternoForma(double px, double py, Forma f);
+void centroDeMassa(Forma f, double *ponto);
+double distanciaCentro(Forma j, Forma k);
+
+// São funções usadas apenas nesse arquivo ?
+bool pontoInternoRetangulo(double px, double py, Retangulo r);
+bool pontoInternoCirculo(double px, double py, Circulo c);
+double distancia(double x1, double y1, double x2, double y2);
+
+#endif
