5bdb7599f759a7920faaa14871a0692eeccdad12
ViniciusLuciano <vini-luciano@hotmail.com>
2019-05-07 21:36:55 -0300

    adicionando objeto cidade, Cidade não é TAD

---
+++ t2/src/Objetos/Cidade/cidade.c
@@ -0,0 +1,88 @@
+#include "cidade.h"
+
+struct cidade {
+    Lista listaForma;
+    Lista listaQuadra;
+    Lista listaHidrante;
+    Lista listaSemaforo;
+    Lista listaRadioBase;
+    Lista listaTexto;
+};
+
+void inicializarCidade() {
+    Cidade cidade = malloc(sizeof(struct cidade));
+    cidade->listaForma = criarLista(1000);
+    cidade->listaQuadra = criarLista(1000);
+    cidade->listaHidrante = criarLista(1000);
+    cidade->listaSemaforo = criarLista(1000);
+    cidade->listaRadioBase = criarLista(1000);
+    cidade->listaTexto = criarLista(1000);
+}
+
+/*
+Objeto getCidade_objeto(Cidade cidade, char *id) {
+    Objeto obj;
+    
+    obj = encontrarObjeto(cidade->listaForma, id, formaEquals);
+    if(obj != NULL)
+        return obj;
+    
+    obj = encontrarObjeto(cidade->listaQuadra, id, quadraEquals);
+    if(obj != NULL)
+        return obj;
+    
+    obj = encontrarObjeto(cidade->listaHidrante, id, hidranteEquals);
+    if(obj != NULL)
+        return obj;
+    
+    obj = encontrarObjeto(cidade->listaSemaforo, id, semaforoEquals);
+    if(obj != NULL)
+        return obj;
+    
+    obj = encontrarObjeto(cidade->listaRadioBase, id, radioBaseEquals);
+    if(obj != NULL)
+        return obj;
+    
+    return NULL;
+}
+*/
+
+void setCidade_Forma(Cidade cidade, Forma forma) {
+    inserirUltimo(cidade->listaForma, forma);
+}
+
+void setCidade_Quadra(Cidade cidade, Quadra quadra) {
+    inserirUltimo(cidade->listaQuadra, quadra);
+}
+
+void setCidade_Hidrante(Cidade cidade, Hidrante hidrante) {
+    inserirUltimo(cidade->listaHidrante, hidrante);
+}
+
+void setCidade_Semaforo(Cidade cidade, Semaforo semaforo) {
+    inserirUltimo(cidade->listaSemaforo, semaforo);
+}
+
+void setCidade_RadioBase(Cidade cidade, RadioBase radioBase) {
+    inserirUltimo(cidade->listaRadioBase, radioBase);
+}
+
+Forma getCidade_Forma(Cidade cidade, char *id) {
+    return encontrarObjeto(cidade->listaForma, id, (void*)formaEquals);
+}
+
+Quadra getCidade_Quadra(Cidade cidade, char *id) {
+    return encontrarObjeto(cidade->listaQuadra, id, (void*)quadraEquals);
+}
+
+Hidrante getCidade_Hidrante(Cidade cidade, char *id) {
+    return encontrarObjeto(cidade->listaHidrante, id, (void*)hidranteEquals);
+}
+
+Semaforo getCidade_Semaforo(Cidade cidade, char *id) {
+    return encontrarObjeto(cidade->listaSemaforo, id, (void*)semaforoEquals);
+}
+
+RadioBase getCidade_RadioBase(Cidade cidade, char *id) {
+    return encontrarObjeto(cidade->listaRadioBase, id, (void*)radioBaseEquals);
+}
---
+++ t2/src/Objetos/Cidade/cidade.h
@@ -0,0 +1,33 @@
+#ifndef CIDADE_H
+#define CIDADE_H
+
+#include "../../headers.h"
+#include "../../Utils/Lista/lista.h"
+#include "../Forma/forma.h"
+#include "../Texto/texto.h"
+#include "../Quadra/quadra.h"
+#include "../Hidrante/hidrante.h"
+#include "../Semaforo/semaforo.h"
+#include "../RadioBase/radioBase.h"
+
+/*
+Uma cidade contem todos os elementos.
+Uma cidade não é um TAD!!
+*/
+
+typedef struct cidade* Cidade;
+
+void inicializarCidade();
+Objeto getCidade_objeto(Cidade cidade, char *id);
+void setCidade_Forma(Cidade cidade, Forma forma);
+void setCidade_Quadra(Cidade cidade, Quadra quadra);
+void setCidade_Hidrante(Cidade cidade, Hidrante hidrante);
+void setCidade_Semaforo(Cidade cidade, Semaforo semaforo);
+void setCidade_RadioBase(Cidade cidade, RadioBase radioBase);
+Forma getCidade_Forma(Cidade cidade, char *id);
+Quadra getCidade_Quadra(Cidade cidade, char *id);
+Hidrante getCidade_Hidrante(Cidade cidade, char *id);
+Semaforo getCidade_Semaforo(Cidade cidade, char *id);
+RadioBase getCidade_RadioBase(Cidade cidade, char *id);
+
+#endif
